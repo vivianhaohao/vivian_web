ef32aba15f933daec996813be5501154db97aa1d
vivianhaohao <vivianhhmm11@gmail.com>
2025-09-07 18:21:45 +0800

    change path

--- src/layoutComponents/head.tsx
+++ src/layoutComponents/head.tsx
@@ -1,5 +1,5 @@
 import {Link} from "react-router-dom";
-import Record from "./music_record.tsx"
+import Record from "../layoutComponents/music_record.tsx"
 
 export default function Head() {
   return (
